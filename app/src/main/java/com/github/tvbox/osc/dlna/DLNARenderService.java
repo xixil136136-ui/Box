7d47d78b8bdf8889c5b598b790801bd89ef9425f
xixil136136-ui <xixil136136@gmail.com>
2026-07-01 08:20:10 +0800

    fix: 重写DLNARenderService(修正导入)

--- app/src/main/java/com/github/tvbox/osc/dlna/DLNARenderService.java
+++ app/src/main/java/com/github/tvbox/osc/dlna/DLNARenderService.java
@@ -5,35 +5,28 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-import org.fourthline.clingg.UpnpService;
-import org.fourthline.clingg.UpnpServiceImpl;
-import org.fourthline.clingg.binding.annotations.AnnotationLocalServiceBinder;
-import org.fourthline.clingg.model.DefaultServiceManager;
+import org.fourthline.cling.UpnpService;
+import org.fourthline.cling.UpnpServiceImpl;
+import org.fourthline.cling.binding.annotations.AnnotationLocalServiceBinder;
+import org.fourthline.cling.model.DefaultServiceManager;
 import org.fourthline.cling.model.meta.DeviceDetails;
-import org.fourthline.clingg.model.meta.DeviceIdentity;
-import org.fourthline.clingg.model.meta.LocalDevice;
-import org.fourthline.clingg.model.meta.LocalService;
-import org.fourthline.clingg.model.meta.ManufacturerDetails;
-import org.fourthline.clingg.model.meta.ModelDetails;
-import org.fourthline.clingg.model.types.DeviceType;
-import org.fourthline.clingg.model.types.UDADeviceType;
-import org.fourthline.clingg.model.types.UDN;
-import org.fourthline.clingg.support.avtransport.impl.AVTransportService;
-import org.fourthline.clingg.support.connectionmanager.impl.ConnectionManagerServiceImpl;
-import org.fourthline.clingg.support.renderingcontrol.impl.RenderingControlServiceImpl;
+import org.fourthline.cling.model.meta.DeviceIdentity;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.LocalService;
+import org.fourthline.cling.model.meta.ManufacturerDetails;
+import org.fourthline.cling.model.meta.ModelDetails;
+import org.fourthline.cling.model.types.DeviceType;
+import org.fourthline.cling.model.types.UDADeviceType;
+import org.fourthline.cling.model.types.UDN;
+import org.fourthline.cling.support.avtransport.impl.AVTransportService;
+import org.fourthline.cling.support.connectionmanager.impl.ConnectionManagerServiceImpl;
+import org.fourthline.cling.support.renderingcontrol.impl.RenderingControlServiceImpl;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 
 /**
  * DLNA 投屏接收端服务
- * 让本机作为 DLNA MediaRenderer，接收手机端腾讯/爱奇艺/B站等APP投屏
- *
- * 在 Application.onCreate() 或 HomeActivity 中启动：
- *   startService(new Intent(context, DLNARenderService.class));
+ * 让本机作为 DLNA MediaRenderer，接收手机端APP投屏
  */
 public class DLNARenderService extends Service {
 
@@ -51,7 +44,7 @@ public class DLNARenderService extends Service {
             upnpService = new UpnpServiceImpl();
             LocalDevice device = createDevice();
             upnpService.getRegistry().addDevice(device);
-            Log.i(TAG, "DLNA Renderer started: 全能看-TVBox");
+            Log.i(TAG, "DLNA Renderer started");
         } catch (Exception e) {
             Log.e(TAG, "DLNA start failed", e);
         }
@@ -67,8 +60,7 @@ public class DLNARenderService extends Service {
 
         LocalService<AVTransportService> avtService =
                 new AnnotationLocalServiceBinder().read(AVTransportService.class);
-        avtService.setManager(new DefaultServiceManager<>(
-                avtService, AVTransportService.class));
+        avtService.setManager(new DefaultServiceManager<>(avtService, AVTransportService.class));
 
         LocalService<ConnectionManagerServiceImpl> cmService =
                 new AnnotationLocalServiceBinder().read(ConnectionManagerServiceImpl.class);
